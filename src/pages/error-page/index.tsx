ee77fe9cf9d3574c922c43d1dd421015e10bf912
eriveltosilva <eriveltoclenio@gmail.com>
2024-09-03 04:51:27 +0100

    fix: building ts error

--- src/pages/error-page/index.tsx
+++ src/pages/error-page/index.tsx
@@ -2,14 +2,27 @@ import { useRouteError } from "react-router-dom";
 
 export function ErrorPage() {
     const error = useRouteError();
-    console.error(error);
 
+    // Verifica se o erro é uma instância de Error
+    if (error instanceof Error) {
+        return (
+            <div id="error-page">
+                <h1>Oops!</h1>
+                <p>Sorry, an unexpected error has occurred.</p>
+                <p>
+                    <i>{error.message}</i>
+                </p>
+            </div>
+        );
+    }
+
+    // Se o erro não for uma instância de Error, lide com outros tipos de erro
     return (
         <div id="error-page">
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
             <p>
-                <i>{error.statusText || error.message}</i>
+                <i>{(error as { statusText?: string }).statusText || "Unknown error"}</i>
             </p>
         </div>
     );
